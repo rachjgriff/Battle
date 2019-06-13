ffe2f4e9c732c55de73bbb4ef1c5f18ea7bcc646
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-13 15:46:08 +0100

    post/redirect/get

--- app.rb
+++ app.rb
@@ -2,13 +2,21 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @Player_1 = params[:Player_1]
-    @Player_2 = params[:Player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb :play
   end
 
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,8 +2,8 @@ feature 'enter names' do
   scenario 'players enter names into form and submit' do
     visit('/')
 
-    fill_in "Player_1", :with  => "Dave"
-    fill_in "Player_2", :with  => "Sarah"
+    fill_in "player_1", :with  => "Dave"
+    fill_in "player_2", :with  => "Sarah"
     click_button "Submit"
 
     expect(page).to have_content "Dave vs. Sarah"
