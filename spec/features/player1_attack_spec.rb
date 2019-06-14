c432ca48c5575d1d75c861aa6eddeafa312308cf
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-14 10:04:45 +0100

    player 1 attacks

---
+++ spec/features/player1_attack_spec.rb
@@ -0,0 +1,7 @@
+feature "attack" do
+  scenario "Player 1 attacks player 2" do
+    sign_in_and_play
+    click_button "ATTACK!"
+    expect(page).to have_content("Dave has attacked Sarah")
+  end
+end
