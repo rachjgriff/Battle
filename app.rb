7813e02ff897ce1992cc938fb9dba48fab86d209
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-14 17:01:51 +0100

    skinny controller

--- app.rb
+++ app.rb
@@ -16,16 +16,12 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = $game.player_1.name
-    @player_2_name = $game.player_2.name
-    @player_1_hit_points = $game.player_1.hit_points
-    @player_2_hit_points = $game.player_2.hit_points
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $game.player_1.name
-    @player_2_name = $game.player_2.name
+    @game = $game
     $game.attack($game.player_2)
     erb :attack
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,7 +4,7 @@ describe Game do
 
   let(:player_1) {double :player_1}
   let(:player_2) {double :player_2}
-  subject(:game) {Game.new(player_1, player_2)}
+  subject(:game) {described_class.new(player_1, player_2)}
 
   it 'initialize game with 2 players' do
     expect(game.player_1).to eq player_1
